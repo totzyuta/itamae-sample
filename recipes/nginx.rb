d4313559ed387161aa9995d9ba690956ea2e1654
totzYuta <yuta.totz@gmail.com>
2015-11-29 14:06:39 +0900

    Add nginx conf files

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -2,6 +2,14 @@ package 'nginx' do
   action :install
 end
 
+remote_file "/etc/nginx/nginx.conf" do
+  source "../remote_files/nginx.conf"
+end
+
+remote_file "/etc/nginx/conf.d/onebox.conf" do
+  source "../remote_files/onebox.conf"
+end
+
 service 'nginx' do
   action [:enable, :start]
 end
