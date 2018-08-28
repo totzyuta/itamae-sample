25c96b4729f8ed3d49901a16cfc4ee3592a3266a
totzyuta <yuta.totz@gmail.com>
2018-08-28 14:01:48 +0900

    Fix nginx conf file

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -6,8 +6,8 @@ remote_file "/etc/nginx/nginx.conf" do
   source "../remote_files/nginx.conf"
 end
 
-remote_file "/etc/nginx/conf.d/onebox.conf" do
-  source "../remote_files/onebox.conf"
+remote_file "/etc/nginx/conf.d/virtual.conf" do
+  source "../remote_files/app.conf"
 end
 
 service 'nginx' do
